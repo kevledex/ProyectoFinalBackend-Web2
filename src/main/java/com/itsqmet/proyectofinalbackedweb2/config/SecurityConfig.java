d54f21def7ca1c9d4e3b0916323c5ab2efeacbf4
kevledex <kevinledesma014@gmail.com>
2026-08-26 21:54:38 -0500

    Ajuste en cors

--- src/main/java/com/itsqmet/proyectofinalbackedweb2/config/SecurityConfig.java
+++ src/main/java/com/itsqmet/proyectofinalbackedweb2/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:5500", "http://127.0.0.1:5500"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:5500", "http://127.0.0.1:5500", "http://192.168.137.1:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
